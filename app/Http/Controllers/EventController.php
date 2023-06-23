1f1313760a7815fe0e0cf2be77521c3742ee659b
carloseduardobanjar <carlosesb@dcc.ufrj.br>
2023-06-23 15:50:28 -0300

    Salvar dados no banco

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,7 +14,21 @@ class EventController extends Controller
         return view('welcome', ['events' => $events]);
     }
 
-    public function create(){
-        return view('events.create');
+    public function create(){ 
+        return view('events.create'); //Retorna a tela de Criar Evento
+    }
+
+    public function store(Request $request) {
+
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        $event->save();
+
+        return redirect("/")->with('msg', 'Evento criado com sucesso!');
     }
 }
